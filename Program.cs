915b8f6ae1a977864ebb0fa02c54aabbccb239bb
Fauze <fauze.cavalari@gmail.com>
2025-02-04 21:46:19 -0300

    adding frame extractor and uploader to s3

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using hacka_video_consumer;
 using hacka_video_consumer.lib.adapters.controllers;
 using hacka_video_consumer.lib.adapters.controllers.ports;
 using hacka_video_consumer.lib.adapters.gateways;
+using hacka_video_consumer.lib.application.services;
+using hacka_video_consumer.lib.application.use_cases;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHostedService<SqsClientRepository>();
@@ -29,6 +31,19 @@ builder.Services.AddSingleton<IAmazonS3>(_ =>
 });
 builder.Services.AddSingleton<S3ClientGateway, S3ClientRepository>();
 builder.Services.AddSingleton<IFrameExtractorControllerPorts, FrameExtractorController>();
+builder.Services.AddSingleton<ExtractVideoFrames>(provider =>
+{
+    var logger = provider.GetRequiredService<ILogger<ExtractVideoFrames>>();
+    return new ExtractVideoFrames("/usr/bin/ffmpeg", logger);
+});
+builder.Services.AddSingleton<UploadFileToS3Async>(provider =>
+{
+    var s3Client = provider.GetRequiredService<IAmazonS3>();
+    var logger = provider.GetRequiredService<ILogger<UploadFileToS3Async>>();
+    string bucketName = "https://localhost.localstack.cloud:4566/frame-extractor-storage/";
+    return new UploadFileToS3Async(s3Client, logger, bucketName);
+});
+builder.Services.AddSingleton<ProcessVideoUseCase>();
 
 var app = builder.Build();
 
---
+++ lib/application/use-cases/ProcessVideoUseCase.cs
@@ -0,0 +1,44 @@
+﻿using hacka_video_consumer.lib.adapters.gateways;
+using hacka_video_consumer.lib.application.use_cases;
+
+namespace hacka_video_consumer.lib.application.services;
+
+public class ProcessVideoUseCase
+{
+    private readonly ExtractVideoFrames _extractor;
+    private readonly UploadFileToS3Async _uploader;
+    private readonly ILogger<ProcessVideoUseCase> _logger;
+
+    public ProcessVideoUseCase(ExtractVideoFrames extractor, UploadFileToS3Async uploader, ILogger<ProcessVideoUseCase> logger)
+    {
+        _extractor = extractor;
+        _uploader = uploader;
+        _logger = logger;
+    }
+
+    public async Task ProcessAsync(string videoPath, string userId, string videoId)
+    {
+        try
+        {
+            _logger.LogInformation($"Processing video: {videoPath}");
+
+            string? zipFilePath = await _extractor.ExtractFramesAsync(videoPath, videoId);
+
+            if (zipFilePath == null)
+            {
+                _logger.LogError("Failed to extract frames. Stopping process.");
+                return;
+            }
+
+            _logger.LogInformation($"Frames extracted and saved to {zipFilePath}");
+
+            await _uploader.UploadFileAsync(zipFilePath, userId, videoId);
+
+            _logger.LogInformation("Process completed successfully.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error processing video: {ex.Message}");
+        }
+    }
+}
