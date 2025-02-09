78b7077c59e30f9650269f56542c833204d093f5
Luiz - Personal <luizgmaker@gmail.com>
2025-02-09 12:05:55 -0300

    fixed processment flow

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
-import path from 'path';
 import { Consumer } from 'sqs-consumer';
-import { SQSClient } from "@aws-sdk/client-sqs";
+import { Message, SQSClient } from "@aws-sdk/client-sqs";
 import { S3ClientRepository } from "./infra/repositories/s3-client.repository";
 import { FfmpegRepository } from "./infra/repositories/ffmpeg.repository";
-import { ZipRepository } from "./infra/repositories/video-frames-zip.repository";
-import { UploadRepository } from "./infra/repositories/upload-s3-client.repository";
+import { ZipRepository } from "./infra/repositories/zip.repository";
+import { VideoProcessingMessage } from './core/entities/video-processing-message';
 
 require('dotenv').config();
 
-const handleMessage = async (message: any) => {
+const handleMessage = async (message: Message) => {
     try {
-        console.log(message);
-        const body = JSON.parse(message.Body);
-        const filePath: string = await S3ClientRepository.download(body.s3Key);
+        console.log(`✉️ Nova mensagem`, message);
+
+        const s3Repository = new S3ClientRepository(
+            process.env.AWS_REGION!,
+            process.env.AWS_ACCESS_KEY_ID!,
+            process.env.AWS_SECRET_ACCESS_KEY!,
+            process.env.AWS_SESSION_TOKEN!
+        );
+        
+        const videoData: VideoProcessingMessage = JSON.parse(message?.Body || "");
+                
+        const filePath: string = await s3Repository.download(
+            process.env.VIDEOS_BUCKET_NAME!,
+            videoData.s3key
+        );
+
         const framesDir = await FfmpegRepository.extractFrames(filePath, 1);
-        const zipPath = path.join(process.cwd(), "temp", "frames.zip");
+        const zipPath = await ZipRepository.zipDirectory(framesDir);
 
-        await ZipRepository.zipDirectory(framesDir, zipPath);
-
-        await UploadRepository.uploadFile({
-            bucket: process.env.VIDEOS_BUCKET_NAME!,
-            key: `processed/${path.basename(zipPath)}`,
+        await s3Repository.uploadFile({
+            bucket: process.env.FRAMES_BUCKET_NAME!,
+            key: `${videoData.userId}/${videoData.videoId}/frames.zip`,
             filePath: zipPath
         });
 
@@ -31,14 +41,14 @@ const handleMessage = async (message: any) => {
 };
 
 const app = Consumer.create({
-    queueUrl: process.env.SQS_QUEUE_URL!,
+    queueUrl: process.env.VIDEO_PROCESSING_QUEUE_URL!,
     handleMessage,
     sqs: new SQSClient({
         region: 'us-east-1',
         credentials: {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-            secretAccessKey: process.env.SECRET_ACCESS_KEY || '',
-            sessionToken: process.env.SESSION_TOKEN || ''
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+            sessionToken: process.env.AWS_SESSION_TOKEN || ''
         }
     })
 });
@@ -51,7 +61,7 @@ app.start();
 
 app.on('error', (err) => {
     console.error(err)
-    console.log(process.env.AWAWS_ACCESS_KEY_ID, process.env.SECRET_ACCESS_KEY, process.env.SESSION_TOKEN)
+    console.log(process.env.AWS_ACCESS_KEY_ID, process.env.SECRET_ACCESS_KEY, process.env.SESSION_TOKEN)
 })
 
 app.on("processing_error", (err) => {
