c16a71cb036817b4cbc683a8f348fed62716fc1d
Fauze <fauze.cavalari@gmail.com>
2025-02-09 00:22:34 -0300

    install ffmpeg on build

--- dockerfile
+++ dockerfile
@@ -1,23 +1,13 @@
+FROM node:20-slim
 
-FROM node:20-bullseye
+RUN apt update && apt install -y ffmpeg
 
 WORKDIR /app
 
-COPY package*.json ./
+COPY . /app
 
 RUN npm install
 
-COPY . .
-
-RUN npm run build
-
-COPY devops/installers/ffmpeg /usr/local/bin/ffmpeg
-COPY devops/installers/ffprobe /usr/local/bin/ffprobe
-
-RUN chmod +x /usr/local/bin/ffmpeg /usr/local/bin/ffprobe
-
-ENV PATH="/usr/local/bin:$PATH"
-
 EXPOSE 3000
 
-CMD ["npm", "run", "start"]
+CMD ["npm", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import path from 'path';
-import { Consumer } from 'sqs-consumer'
-import {SQSClient} from "@aws-sdk/client-sqs";
-import {S3ClientRepository} from "./infra/repositories/s3-client.repository";
-import {FfmpegRepository} from "./infra/repositories/ffmpeg.repository";
+import { Consumer } from 'sqs-consumer';
+import { SQSClient } from "@aws-sdk/client-sqs";
+import { S3ClientRepository } from "./infra/repositories/s3-client.repository";
+import { FfmpegRepository } from "./infra/repositories/ffmpeg.repository";
+import { ZipRepository } from "./infra/repositories/video-frames-zip.repository";
+import { UploadRepository } from "./infra/repositories/upload-s3-client.repository";
+
 require('dotenv').config();
 
 const handleMessage = async (message: any) => {
-    console.log(message)
-    const body = JSON.parse(message.Body)
-    const filePath: string = await S3ClientRepository.download(body.s3Key)
-    await FfmpegRepository.extractFrames(filePath, 1)
-}
+    try {
+        console.log(message);
+        const body = JSON.parse(message.Body);
+        const filePath: string = await S3ClientRepository.download(body.s3Key);
+        const framesDir = await FfmpegRepository.extractFrames(filePath, 1);
+        const zipPath = path.join(process.cwd(), "temp", "frames.zip");
+
+        await ZipRepository.zipDirectory(framesDir, zipPath);
+
+        await UploadRepository.uploadFile({
+            bucket: process.env.VIDEOS_BUCKET_NAME!,
+            key: `processed/${path.basename(zipPath)}`,
+            filePath: zipPath
+        });
+
+        console.log("✅ Processo finalizado com sucesso!");
+    } catch (error) {
+        console.error("❌ Erro no processamento:", error);
+    }
+};
 
 const app = Consumer.create({
-    queueUrl: 'https://sqs.us-east-1.amazonaws.com/870947314561/frame-extractor-queue',
+    queueUrl: process.env.SQS_QUEUE_URL!,
     handleMessage,
     sqs: new SQSClient({
         region: 'us-east-1',
@@ -23,7 +41,13 @@ const app = Consumer.create({
             sessionToken: process.env.SESSION_TOKEN || ''
         }
     })
-})
+});
+
+app.on('error', (err) => console.error(err));
+app.on("processing_error", (err) => console.error(err));
+
+app.start();
+
 
 app.on('error', (err) => {
     console.error(err)
